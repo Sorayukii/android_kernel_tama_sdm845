a2b993290451b21f8567d5f0e83e34a61e7564c1
EmanuelCN <emanuelghub@gmail.com>
2023-10-09 21:51:10 +0300

    kernel: sched: Remove check_for_migration()
    This calls select_energy_cpu_brute() which CASS has an alternative and will take care of, so remove it we leave the job to CASS.
    
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2610,8 +2610,6 @@ void scheduler_ipi(void)
 	if (got_boost_kick()) {
 		struct rq *rq = cpu_rq(cpu);
 
-		if (rq->curr->sched_class == &fair_sched_class)
-			check_for_migration(rq, rq->curr);
 		clear_boost_kick(cpu);
 	}
 
@@ -4032,8 +4030,6 @@ void scheduler_tick(void)
 		set_preferred_cluster(grp);
 	rcu_read_unlock();
 
-	if (curr->sched_class == &fair_sched_class)
-		check_for_migration(rq, curr);
 }
 
 #ifdef CONFIG_NO_HZ_FULL
