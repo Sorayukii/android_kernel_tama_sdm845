214ff15115686e542d1015a61ddd342cb2cadf6d
Sorayukii Ify <rybutajaya@gmail.com>
2024-12-25 04:18:31 +0000

    sched/uclamp: Tune Uclamp assist

--- kernel/cpuset.c
+++ kernel/cpuset.c
@@ -2035,12 +2035,12 @@ static void uclamp_set(struct kernfs_open_file *of,
 	const char *cs_name = cs->css.cgroup->kn->name;
 
 	static struct ucl_param tgts[] = {
-		{"top-app",    	     	"10", "100", 1, 1},
-		{"foreground", 	     	"0",  "50",  1, 1},
-		{"restricted", 	     	"0",  "40",  0, 0},
-		{"background", 	     	"20", "100", 0, 0},
-		{"camera-daemon",		"0",  "40",  0, 0},
-		{"system-background", 	"0",  "40",  0, 0},
+		{"top-app",    	     	"10", "max", 1, 1},
+		{"foreground", 	     	"20", "50",  0, 0},
+		{"restricted", 	     	"0",  "20",  0, 0},
+		{"background", 	     	"20", "max", 0, 0},
+		{"camera-daemon",		"0",  "20",  0, 0},
+		{"system-background", 	"10", "50",  0, 0},
 	};
 
 	for (i = 0; i < ARRAY_SIZE(tgts); i++) {
