cf9a083f7d0d30435f128a3a0cb17d442d87a3d5
kanonifyX <kanonify01@gmail.com>
2024-12-25 14:22:03 +0700

    sched/uclamp: Tune uclamp assist
    Reference values from darkhz/uclamp@586d157
    
    Signed-off-by: KanonifyX <kanonify01@gmail.com>

--- kernel/cpuset.c
+++ kernel/cpuset.c
@@ -2037,10 +2037,10 @@ static void uclamp_set(struct kernfs_open_file *of,
 	const char *cs_name = cs->css.cgroup->kn->name;
 
 	static struct ucl_param tgts[] = {
-		{"top-app",    	     	"10", "100", 1, 1},
-		{"foreground", 	     	"0",  "50",  1, 1},
-		{"background", 	     	"20", "100", 0, 0},
-		{"system-background", 	"0",  "40",  0, 0},
+		{"top-app",    	     	"20", "max", 1, 1},
+		{"foreground", 	     	"20", "50",  0, 0},
+		{"background", 	     	"10", "max", 0, 0},
+		{"system-background", 	"10", "50",  0, 0},
 	};
 
 	for (i = 0; i < ARRAY_SIZE(tgts); i++) {
@@ -2062,7 +2062,7 @@ static void uclamp_set(struct kernfs_open_file *of,
 
 	/* Set systemwide uclamps */
 	sysctl_sched_uclamp_util_min = 128;
-	sysctl_sched_uclamp_util_min_rt_default = 500;
+	sysctl_sched_uclamp_util_min_rt_default = 96;
 }
 #endif
 
